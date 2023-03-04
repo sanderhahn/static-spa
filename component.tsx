5e9f45390d17f784fab5eab3d939e7c86b0d055f
Sander Hahn <sanderhahn@gmail.com>
2023-03-05 00:35:47 +0100

    make build work

--- component.tsx
+++ component.tsx
@@ -1,3 +1,12 @@
+import React from 'react';
+import { replacer, Reviver } from 'json-utils';
+
+console.log(JSON.stringify({
+    a: 1n,
+}, replacer));
+
+const r = Reviver({});
+
 export default function Component() {
     return <div>Component</div>;
 }
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
+// JavaScript file so that loading doesn't require ts-node
 /** @type {import('jest').Config} */
 const config = {
   verbose: true,
   testMatch: ['**/*_test.tsx'],
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
+  resolver: 'ts-jest-resolver',
 };
 
 export default config;
